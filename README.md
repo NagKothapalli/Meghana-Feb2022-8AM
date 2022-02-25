400de37eba5a182a37a0b9bd48e8b2ea81f1b7fb
NagKothapalli <nag022@gmail.com>
2022-02-25 08:31:30 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Meghana-Feb2022-8AM
+ Repo for Daily Selenium Java COde
---
+++ src/coreConcepts/BasicConcepts.java
@@ -0,0 +1,88 @@
+package coreConcepts;
+
+//Signature : CLASS
+ // Modifier   class  Name{ Body : Members -> variables / functions  }
+ //Access Modifiers : private, default , protected , public
+/*Non Access Modifiers : static , final
+ Class Name should always start with UpperCase , No Spaces within the class , No special characters*/
+public class BasicConcepts
+{
+  //Signature : variable
+	//Modifier[opt]   DataType  name; - Declaration   ||  Modifier[opt]   DataType  name  = value ; - Assignment
+	public static final String collegeName = "JNTU - HYD";
+	public     int     stdNum   = 22;  // byte - 8 short - 16  int - long
+	private  double    stdMarks = 54.67;  //1,2,3,4,5,6,77,777,45678,.......
+	         char      stdGrade = 'A'; // abcdefg.....z
+	         boolean   stdResult = true;
+	//Modifier[opt]  ReturnType Name(arguments[opt]){ body }
+	            // NO - void        //DataType arg1,DataType arg2, .......
+	            // YES - int,String , char , boolean
+	 //psvm
+	 public static void main(String[] args) //class reader
+	 {
+		System.out.println("Welcome to Selenium Java"); 
+		addition();
+		subtraction();
+		multiplication();
+		division();
+	 }
+	 int empNum;
+	 //int empNum;
+	 public static void addition() //normal student
+	 {
+		 int sum = 10 + 20 ;//int+int=int / int+string | string+int | string+string = string
+		 System.out.println("Sum of two numbers :"+sum); //syso  CTRL+SPACE
+	 }
+	 public static void subtraction()
+	 {
+		 int diff = 30-20;
+		 System.out.println("Difference of two numbers :" + diff);
+	 }
+	 public static void multiplication()
+	 {
+		 int prod = 30 * 20; // int
+		 System.out.println("Product of two numbers :" + prod);
+	 }
+	 public static void division()
+	 {
+		 int div = 30/20;
+		 System.out.println("Division of two numbers :" + div);
+	 }
+	
+	
+}
+
+
+//Signature : Method
+	//Modifier[opt]  ReturnType  Name(arguments[opt]){ body }
+	         
+	         //[] - square bracket
+	         //() - Parenthesis
+	         //{ } - curly / flower 
+	
+	//openDoor()  - there is only single door
+	//openDoor(5)  - there are 10 rooms - 1 , 2, 3 ,4 , 5 .......
+	/*
+	 * private void printCollegeName() { //System.out.println("JNTU - H");
+	 * System.out.println(collegeName); }
+	 */
+
+class Yahoo   //Yahoo.java
+{
+	private void printCollegeName() {
+		
+		//System.out.println("JNTU - H");
+		System.out.println(BasicConcepts.collegeName);
+		//BasicConcepts.collegeName = "JNTU - HYDERABAD";
+
+	}
+}
+
+
+class Amazon
+{
+private void printCollegeName() {		
+		//System.out.println("JNTU - K");
+		System.out.println(BasicConcepts.collegeName);
+	}
+}
